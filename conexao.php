0ecb92765a6a5e7f0678d217c95459164d47cb0d
Marcus <marcustec.inf@hotmail.com>
2019-08-29 21:52:32 -0300

    Login pronto

--- conexao.php
+++ conexao.php
@@ -1,45 +1,32 @@
 <html>
     <title>Login</title>
-    <body>
-        <?php
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
-        
-        $conn = mysqli_connect($servername, $username, $password, $database);
-        
-        SELECT * FROM usuario WHERE email = "{$email}" AND senha = "{$senha}";
-        
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
-
-        
-        //conexão com o bando de dados
-        
-        //Fazer query no banco de dados
-        SELECT * FROM usuario WHERE email = "{$email}" AND senha = "{$senha}";
-        
-        //SE o resultado for verdadeiro
-            //cria sessão
-        //SE for falto volta para login (com mensagem de erro)
-        
     
-        
-        session_start();
-        
-        if("QUERY RETORNOU POSITIVO"){
-            echo "password accepted";
-            $_SESSION['login']=true;
-        }else {
-            echo "incorrect login";
-        }
-        ?>
+<body>
 
-        <form name="input" action="adminportal.php" method="get">
-            <input type="submit" value="Home">
-        </form>
-    </body>
+<?php
+session_start();
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+ 
+$con = mysqli_connect("localhost","root","","jf") or die ("Sem conexão com o servidor");
+
+$sql = mysqli_query($con, "SELECT * FROM usuario WHERE email = '$email' AND senha = SHA1('$senha')");
+
+if(mysqli_num_rows($sql) == 1 )
+{
+$_SESSION['email'] = $email;
+$_SESSION['senha'] = $senha;
+header('location:orcamento.php');
+}
+else{
+unset ($_SESSION['login']);
+unset ($_SESSION['senha']);
+header('location:ok.php');
+  
+}
+
+?>
+    
+</body>
 </html>
