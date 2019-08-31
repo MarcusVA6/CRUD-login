853d4719387fa599b5d57b6486600c93d2a55fd9
MARCUS VINICIUS ARAGÃO MENDES <MARCUS.MENDES@csi.aneas.msft>
2019-08-30 21:54:39 -0300

    Início do CRUD de cadastro

--- conexao.php
+++ conexao.php
@@ -1,32 +1,3 @@
-<html>
-    <title>Login</title>
-    
-<body>
-
 <?php
-session_start();
 
-$email = $_POST['email'];
-$senha = $_POST['senha'];
- 
 $con = mysqli_connect("localhost","root","","jf") or die ("Sem conexão com o servidor");
-
-$sql = mysqli_query($con, "SELECT * FROM usuario WHERE email = '$email' AND senha = SHA1('$senha')");
-
-if(mysqli_num_rows($sql) == 1 )
-{
-$_SESSION['email'] = $email;
-$_SESSION['senha'] = $senha;
-header('location:orcamento.php');
-}
-else{
-unset ($_SESSION['login']);
-unset ($_SESSION['senha']);
-header('location:ok.php');
-  
-}
-
-?>
-    
-</body>
-</html>
---
+++ verificacao.php
@@ -0,0 +1,32 @@
+<html>
+    <title>Login</title>
+    
+<body>
+
+<?php
+session_start();
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+ 
+include_once "conexao.php";
+
+$sql = mysqli_query($con, "SELECT * FROM usuario WHERE email = '$email' AND senha = SHA1('$senha')");
+
+if(mysqli_num_rows($sql) == 1 )
+{
+$_SESSION['email'] = $email;
+$_SESSION['senha'] = $senha;
+header('location:orcamento.php');
+}
+else{
+unset ($_SESSION['login']);
+unset ($_SESSION['senha']);
+header('location:ok.php');
+  
+}
+
+?>
+    
+</body>
+</html>
